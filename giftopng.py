1c75b499eebbeaaecde2adcc0dba4a0b164f137d
Jean Loup Pecquais <jeanlouppecquais@ik.me>
2023-02-21 16:18:21 +0100

    clean python script

--- giftopng.py
+++ giftopng.py
@@ -7,11 +7,8 @@ extensions = '.gif'
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        #ext = file.endswith(".gif") #os.path.splitext(file)[-1].lower()
-        #print(ext)
-        if file.endswith(".gif"): #ext in extensions:
+        if file.endswith(".gif"):
             print (os.path.join(subdir, file))
-
             gif=os.path.join(subdir, file)
             img = Image.open(gif)
             img.save(gif.removesuffix('.gif')+".png",'png', optimize=True, quality=100)
